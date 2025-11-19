46f490035bbb4211ce636db38096a6c39cb704f7
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-11-19 20:54:22 +0530

    Minimum Difference Between Largest and Smallest Value in three moves

--- NeetCode-150/src/main/java/org/greedy/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ NeetCode-150/src/main/java/org/greedy/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -1,4 +1,17 @@
 package org.greedy;
 
+import java.util.Arrays;
+
 public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
+    public int minDifference(int[] nums) {
+        if(nums.length <= 4){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.min(
+                Math.min(nums[n-1] - nums[3], nums[n-2] - nums[2]),
+                Math.min(nums[n-3] - nums[1], nums[n-4] - nums[0])
+        );
+    }
 }
