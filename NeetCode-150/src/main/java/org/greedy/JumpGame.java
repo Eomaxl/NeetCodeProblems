fdb2b3a638e2434ed33a87a88e83454f6ba213a0
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-11-19 16:38:34 +0530

    Jump Game I

--- NeetCode-150/src/main/java/org/greedy/JumpGame.java
+++ NeetCode-150/src/main/java/org/greedy/JumpGame.java
@@ -1,4 +1,14 @@
 package org.greedy;
 
 public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if(i + nums[i] >= goal){
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
 }
