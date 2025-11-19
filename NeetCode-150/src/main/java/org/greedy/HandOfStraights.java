07d4b288224f552b5ad495cd6655623bc35f6dc2
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-11-19 19:13:37 +0530

    Hand Of Straight

--- NeetCode-150/src/main/java/org/greedy/HandOfStraights.java
+++ NeetCode-150/src/main/java/org/greedy/HandOfStraights.java
@@ -1,4 +1,42 @@
 package org.greedy;
 
 public class HandOfStraights {
+
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+
+        HashMap<Integer, Integer> cardCount = new HashMap<>();
+        for (int card : hand) {
+            int count = cardCount.getOrDefault(card, 0);
+            cardCount.put(card, count + 1);
+        }
+
+        for (int card : hand) {
+            int startCard = card;
+            // Find the start of the potential straight sequence
+            while (cardCount.getOrDefault(startCard - 1, 0) > 0) {
+                startCard--;
+            }
+
+            // Process the sequence starting from startCard
+            while (startCard <= card) {
+                while (cardCount.getOrDefault(startCard, 0) > 0) {
+                    // Check if we can form a consecutive sequence of groupSize cards
+                    for (
+                            int nextCard = startCard;
+                            nextCard < startCard + groupSize;
+                            nextCard++
+                    ) {
+                        if (cardCount.getOrDefault(nextCard, 0) == 0) {
+                            return false;
+                        }
+                        cardCount.put(nextCard, cardCount.get(nextCard) - 1);
+                    }
+                }
+                startCard++;
+            }
+        }
+
+        return true;
+    }
 }
