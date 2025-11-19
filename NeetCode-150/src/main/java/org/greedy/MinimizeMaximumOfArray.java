4ab92256f96415d5cc3abcd8c8612e78d9af1ddb
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-11-19 19:27:57 +0530

    Minimize Maximum of Array

--- NeetCode-150/src/main/java/org/greedy/MinimizeMaximumOfArray.java
+++ NeetCode-150/src/main/java/org/greedy/MinimizeMaximumOfArray.java
@@ -1,4 +1,16 @@
 package org.greedy;
 
 public class MinimizeMaximumOfArray {
+    public int minimizeArrayValue(int[] nums){
+        int result = 0;
+        long prefix = 0L;
+
+        for (int i = 0; i< nums.length; i++) {
+            prefix += nums[i];
+
+            int needed = (int)(prefix + i)/(i+1);
+            result = Math.max(needed, result);
+        }
+        return result;
+    }
 }
