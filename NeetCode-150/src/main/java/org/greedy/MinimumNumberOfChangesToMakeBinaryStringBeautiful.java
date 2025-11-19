d536ef12af6c6556522d7b1a65ca9a80b1ecfe87
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-11-19 19:21:36 +0530

    Minimum number of changes to make binary string beautiful

--- NeetCode-150/src/main/java/org/greedy/MinimumNumberOfChangesToMakeBinaryStringBeautiful.java
+++ NeetCode-150/src/main/java/org/greedy/MinimumNumberOfChangesToMakeBinaryStringBeautiful.java
@@ -1,4 +1,17 @@
 package org.greedy;
 
 public class MinimumNumberOfChangesToMakeBinaryStringBeautiful {
+
+    public int minChanges(String s){
+        int minChanges = 0;
+
+        for (int i = 0; i < s.length(); i += 2){
+            if(s.charAt(i) != s.charAt(i + 1)){
+                minChanges++;
+            }
+        }
+        return minChanges;
+    }
 }
+
+// TC: O(n), SC : O(1)
