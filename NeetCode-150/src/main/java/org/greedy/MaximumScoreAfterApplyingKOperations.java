b9e581df22d74a636f11b271b97ba7bd16b1a727
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-11-20 21:39:19 +0530

    Maximum score after applying k operations

---
+++ NeetCode-150/src/main/java/org/greedy/MaximumScoreAfterApplyingKOperations.java
@@ -0,0 +1,20 @@
+package org.greedy;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaximumScoreAfterApplyingKOperations {
+    public long maxKelements(int[] arr, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int n = arr.length;
+        long answer = 0;
+        for(int i=0; i<n; i++)  queue.add(arr[i]);
+        for(int i=0; i<k; i++){
+            int temp = queue.poll();
+            answer += temp;
+            double s = (double)temp/3;
+            queue.add((int)Math.ceil(s));
+        }
+        return answer;
+    }
+}
