12cc8d22decac6ee492ad26b824eb757f9dc3891
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2026-01-14 23:58:56 +0530

    Implemented the Node and doubly linked list class

---
+++ LocalPractice/src/main/java/lld/easy/lrucache/Node.java
@@ -0,0 +1,12 @@
+package lld.easy.lrucache;
+
+class Node<K,V> {
+    K key;
+    V value;
+    Node<K, V> prev, next;
+
+    Node(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+}
